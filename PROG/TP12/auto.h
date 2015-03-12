b062f60fdda73e13afa333b4232f59c7b8c77920
AmarOk1412 <amarok@enconn.fr>
2015-03-12 12:54:50 +0100

    auto/bus

---
+++ PROG/TP12/auto.h
@@ -0,0 +1,33 @@
+// -*- c++ -*-
+#ifndef _AUTO_H
+#define _AUTO_H
+
+#include "vehicule.h"
+#include <iostream>
+/**
+ * définition abstraite d'un Vehicule
+ */
+
+class Auto : public Vehicule {
+public:
+  /** @param lg : longueur du véhicule
+      @param nbp : nombre de personnes transportées
+  */
+  Auto(unsigned int nbp = 1, bool tT=false);
+  virtual Vehicule* Clone() const;
+
+  /// destructeur
+  ~Auto(void);
+
+  //! déterminer le tarif du véhicule
+  double	calculerTarif(void) const;
+
+  //! afficher les caractéristiques du véhicule
+  void		afficher(std::ostream & s = std::cout) const;
+private:
+  unsigned int longueur;		///< longueur du véhicule
+  unsigned int passagers;		///< nombre de personnes dans le véhicule
+  bool toutTerrain; 				///tout terrain ou pas
+};
+
+#endif // _VEHICULE_H
---
+++ PROG/TP12/bus.h
@@ -0,0 +1,33 @@
+// -*- c++ -*-
+#ifndef _BUS_H
+#define _BUS_H
+
+#include "vehicule.h"
+#include <iostream>
+/**
+ * définition abstraite d'un Vehicule
+ */
+
+class Bus : public Vehicule {
+public:
+  /** @param lg : longueur du véhicule
+      @param nbp : nombre de personnes transportées
+  */
+  Bus(unsigned int l = 1, unsigned int nbp = 1);
+  
+  virtual Vehicule* Clone() const;
+
+  /// destructeur
+  ~Bus(void);
+
+  //! déterminer le tarif du véhicule
+  double	calculerTarif(void) const;
+
+  //! afficher les caractéristiques du véhicule
+  void		afficher(std::ostream & s = std::cout) const;
+private:
+  unsigned int longueur;		///< longueur du véhicule
+  unsigned int passagers;		///< nombre de personnes dans le véhicule
+};
+
+#endif // _BUS_H
