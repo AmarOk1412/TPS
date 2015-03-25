45ef832eb082f86bdd52b06e14a5585dc3becd6d
PH <pierrehenri.collin@gmail.com>
2015-03-25 17:20:31 +0100

    plugins

---
+++ SYS/C/p2/plugins/show_image_plugin.cc
@@ -0,0 +1,27 @@
+#include <cv.h>
+#include <stdio.h>
+#include "plugin.h"
+#include "pluginmanager.h"
+
+extern "C" {
+
+	void display_filter (cv:: Mat image)
+	{
+	
+		namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+		imshow( "Display window", image );                   // Show our image inside it.
+
+		waitKey(0);                                          // Wait for a keystroke in the window
+   
+	}
+
+  // enregistrer le plugin
+  void
+  init_(plugin_manager * pm)
+  {
+    register_plugin(pm,
+		    "display_filter",
+		    "Affiche une image",
+		    display_filter);
+  }
+};
