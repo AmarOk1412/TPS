c7f17d95c12fef4c821c916fd9884f0020f67392
AmarOk1412 <amarok@enconn.fr>
2015-03-25 12:08:36 +0100

    begin TP4

---
+++ BDD/TP4/init.sql
@@ -0,0 +1,9 @@
+INSERT INTO Cours values(1,"BDD");
+INSERT INTO Enseignant values(1,"Miklos","Zoltan");
+INSERT INTO Etudiant values(1,"Blin","Sébastien", 1);
+INSERT INTO Etudiant values(2,"Collin","Pierre-Henri", 1);
+INSERT INTO Salle values(1);
+INSERT INTO Enseigne values(1,1);
+INSERT INTO estDans values(1,1, date('2003-12-31 01:02:03'));
+INSERT INTO estInscrit values(1,1);
+INSERT INTO Parcours values(1,"ESIR");
