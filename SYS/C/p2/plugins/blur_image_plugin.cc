51c0351fe753fc37d6389ec8d449dd34aac7beb6
PH <pierrehenri.collin@gmail.com>
2015-03-25 17:50:33 +0100

    blur plugin

--- SYS/C/p2/plugins/blur_image_plugin.cc
+++ SYS/C/p2/plugins/blur_image_plugin.cc
@@ -5,9 +5,21 @@
 
 extern "C" {
 
-	void blur_filter (cv:: Mat image)
+	void blur_filter (Char[] filename)
 	{
 	
+		 cv::Mat image = imread(filename);
+		 image = imread( imageName, 1 );
+
+
+		 cv::Mat blur_image; //image résultat
+     cv::blur(image,blur_image,cv::Size(5,5)); //appliquer l'effet de fou sur l'image
+		 imwrite( "Blur_Image.jpg", blur_image ); //enregistrer l'image sur le dd
+
+
+		 return 0;
+		}
+
 	}
 
   // enregistrer le plugin
