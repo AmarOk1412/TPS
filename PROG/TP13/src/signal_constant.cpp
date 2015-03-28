716afcfab6e8adbd703fc5dd6ce62387fbafc0d5
AmarOk1412 <amarok@enconn.fr>
2015-03-28 19:21:47 +0100

    Makefile --"

--- PROG/TP13/src/signal_constant.cpp
+++ PROG/TP13/src/signal_constant.cpp
@@ -2,29 +2,14 @@
 #include "impl_flot.h"
 #include <iostream>
 
-signal_constant::signal_constant(double outVal) : producteur(), _outVal(outVal), _sorties(new std::vector< counted_ptr<flot> >())
-{
-	impl_flot* f = new impl_flot();
-	counted_ptr<flot> sortie(f);
-	_sorties->push_back(sortie);
-}
+signal_constant::signal_constant(double outVal) : producteur_base(1), _outVal(outVal)
+{ }
 
 signal_constant::~signal_constant() {}
-
-unsigned int signal_constant::nbSorties() const
-{
-	return _sorties->size();
-}
-
-const counted_ptr<flot> & signal_constant::getSortie(unsigned int numsortie) const
-{
-	assert(numsortie<nbSorties());
-	return _sorties->at(numsortie);
-}
 
 void signal_constant::calculer()
 {
   unsigned int nb = nbSorties();
 	for(unsigned int i = 0; i < nb; ++i)
-		_sorties->at(i)->inserer(_outVal);
+		_sorties.at(i)->inserer(_outVal);
 }
