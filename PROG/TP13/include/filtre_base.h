241ad1cc3f119be3252762d1e0c0e3706385aed7
PH <pierrehenri.collin@gmail.com>
2015-04-02 10:36:47 +0200

    filtre_base

---
+++ PROG/TP13/include/filtre_base.h
@@ -0,0 +1,16 @@
+
+#ifndef __FILTRE_BASE__H__
+#define __FILTRE_BASE__H__
+
+#include "filtre.h"
+#include "producteur_base.h"
+#include "consommateur_base.h"
+
+class filtre_base : public virtual filtre, public producteur_base, public consommateur_base {
+public:
+	filtre_base(unsigned int nbEntrees, unsigned int nbSorties);
+	virtual ~filtre_base() {}
+	virtual void calculer() = 0;
+}
+
+#endif
---
+++ PROG/TP13/src/filtre_base.cpp
@@ -0,0 +1,4 @@
+#include "filtre_base.h"
+
+filtre_base::filtre_base(unsigned int nbEntrees, unsigned int nbSorties) : producteur_base(nbSorties), consommateur_base(nbEntrees)
+{}
