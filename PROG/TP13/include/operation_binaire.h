924af7ec1601a44a35b1a41a47564370663da5c4
PH <pierrehenri.collin@gmail.com>
2015-04-02 12:09:50 +0200

    operation binaire

--- PROG/TP13/include/operation_binaire.h
+++ PROG/TP13/include/operation_binaire.h
@@ -7,22 +7,22 @@
 template<class T>
 class operation_binaire : public filtre_base{
 	public:
-	operation_binaire(T binaire);
+	operation_binaire();
 	virtual ~operation_binaire();
-	virtual void calculer();
-	private:
+	virtual void calculer()
+	{
+		if(yaDesEchantillons())	_sorties[0]->inserer(bin(_entrees[0]->extraire(),_entrees[1]->extraire()));
+	}		
+	private:	
 	T bin;
 };
 
 template<class T>
-operation_binaire::operation_binaire(T binaire) : bin(binaire), filtre_base(2, 1)
+operation_binaire<T>::~operation_binaire() {};
+template<class T>
+operation_binaire<T>::operation_binaire() : bin(T()), filtre_base(2, 1)
 {}
 
-template<class T>
-void operation_binaire::calculer()
-{
-	if(yaDesEchantillons())	_sorties[0]->inserer(T(_entrees[O],entrees[1]));
-}		
 
 
 #endif
