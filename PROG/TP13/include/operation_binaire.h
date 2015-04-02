e7a5837f136e8b4c1865934986e318034294a015
AmarOk1412 <amarok@enconn.fr>
2015-04-02 11:20:21 +0200

    it's generic bitss

--- PROG/TP13/include/operation_binaire.h
+++ PROG/TP13/include/operation_binaire.h
@@ -13,4 +13,16 @@ class operation_binaire : public filtre_base{
 	private:
 	T bin;
 };
+
+template<class T>
+operation_binaire::operation_binaire(T binaire) : bin(binaire), filtre_base(2, 1)
+{}
+
+template<class T>
+void operation_binaire::calculer()
+{
+	if(yaDesEchantillons())	_sorties[0]->inserer(T(_entrees[O],entrees[1]));
+}		
+
+
 #endif
--- PROG/TP13/src/Volume.cpp
+++ PROG/TP13/src/Volume.cpp
@@ -1,16 +1,22 @@
 #include "Volume.h"
 
-Volume::Volume(double volume)
+Volume::Volume(double volume) : filtre_base(1, 1), _s(volume), _m()
 {
-
+	_m.connecterEntree(_s.getSortie(0), 0);
+	_m.connecterEntree(getSortie(0), 1);
 }
 
-void VolumesetVolume(double volume)
+void Volume::setVolume(double volume)
 {
-
+	_s.setOutVal(volume);
 }
 
 void Volume::calculer()
 {
-
+	if(yaDesEchantillons())
+	{
+		_s.calculer();
+		_m.calculer();
+		_sorties[0]->inserer(_m.getSortie(0)->extraire());
+	}
 }
