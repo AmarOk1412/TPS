3e76ae3e9c2009240b0bd18e957127e10e4965a0
AmarOk1412 <amarok@enconn.fr>
2015-04-05 13:51:08 +0200

    continue lecteur

--- PROG/TP13/src/lecteur_fichier.cpp
+++ PROG/TP13/src/lecteur_fichier.cpp
@@ -1,24 +1,46 @@
 #include "lecteur_fichier.h"
 #include "impl_flot.h"
+#include "composant_exception.h"
+
+#include <cstring>
+#include <memory>
+#include <utility>
+#include <string>
 
 lecteur_fichier::lecteur_fichier(std::string nom, unsigned int nbCanaux) : producteur_base(nbCanaux), _nom(nom), _nbCanaux(nbCanaux)
 {
 	char * cnom = new char [nom.length()+1];
   std::strcpy (cnom, nom.c_str());
-  _isf(cnom, std::ifstream::in | std::ifstream::binary);
-}			
+  _ifs(cnom, std::ios::binary);
+  if(!_ifs.good())
+  {
+  	composant_exception exc("Fichier non trouvé");
+  	exc.what();
+  }
+}
 
 lecteur_fichier::~lecteur_fichier()
-{}
+{ _ifs.close(); }
 
-lecteur_fichier::void calculer()
+void lecteur_fichier::calculer()
 {
-		short int echantillon(0);
-		_isf.read(echantillon, 16);
-		_sorties[0]->insérer(echantillon);
-		if(_nbCanaux==2)
+		if(!_ifs.eof())
 		{
-			_isf.read(echantillon, 16);
-				_sorties[1]->insérer(echantillon);
+			short int echantillon(0);
+			for(unsigned int i = 0; i < _nbCanaux; ++i)
+			{
+				_ifs.read(echantillon, 16);
+				if(!_ifs.good())
+				{
+					composant_exception exc("Échec de lecture");
+					exc.what();
+				}
+				_sorties[i]->inserer(echantillon);
+			}
+		}
+		else
+		{
+			composant_exception exc("Fin de fichier atteint");
+			exc.what();
 		}
 }
