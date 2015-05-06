f53ca3237692719acd9596540f42a07bdbf3d8cd
AmarOk1412 <amarok@enconn.fr>
2015-05-06 16:01:47 +0200

    comment

--- ALG/GRA/TP1/src/TP1.java
+++ ALG/GRA/TP1/src/TP1.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintStream;
 
 
@@ -10,13 +9,18 @@ public class TP1
 {
 	public static Graphe fermeture(Graphe g) throws Exception
 	{
+		//Algorithme de Floyd-Warshall
 		int nbSommet = g.nmax();
 		for(int k = 1; k<nbSommet; ++k)
 			for(int i = 1; i<nbSommet; ++i)
 				for(int j = 1; j<nbSommet; ++j)
+					//Partie routage
 					if(g.validArc(i, k) && g.validArc(k, j))
+						//Si on a une valeur plus petite pour y aller, on la met
+						//Vérifier si l'arc est déjà marqué
 						if(g.valArc(i, k) instanceof ValEnt)
 						  g.ajoutArc(i, j, g.valArc(i, k));
+						//Sinon on met k
 						else
 						  g.ajoutArc(i, j, new ValEnt(k));
 		return g;
@@ -27,6 +31,7 @@ public class TP1
 		Graphe liaisons = new GrapheDicoSucc(10); // initialisation d'un graphe
 		try
 		{
+			//Fichier d'entrée
 			File file = new File("graphe");
 			FileInputStream fis = null;
 
@@ -36,28 +41,31 @@ public class TP1
 				e.printStackTrace();
 			}
 
+			//Création du graphe
 			System.out.println("Entrer la représentation de votre graphe suivant le format \"successeurs\"") ;	
 			liaisons.lireSuccs(fis, new Val(), new Val());
 
 			System.out.println("FICHIER GRAPHE LU");
 			System.out.println("\nReprésentation du graphe suivant le format \"arcs explicites\"") ;
-
+			//Fichier de sortie
 			PrintStream fop = null;
 			try {
 				fop = new PrintStream(new FileOutputStream("grapheOut"));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			//Écriture du graphe
 			liaisons.ecrireArcs(fop);
 			fis.close();
 			System.out.println("FICHIER GRAPHEOUT ÉCRIT");
-
+			//fermeture transitive
 			fermeture(liaisons);
 			try {
 				fop = new PrintStream(new FileOutputStream("fermetureGraphe"));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			//écriture et libération
 			liaisons.ecrireArcs(fop);
 			fis.close();
 			System.out.println("FICHIER FERMETUREGRAPHE ÉCRIT");
