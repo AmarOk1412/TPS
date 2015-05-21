7d437f6b012d973c886131a352faaba8f40f0e4a
AmarOk1412 <amarok@enconn.fr>
2015-05-21 10:59:50 +0200

    create process

--- ALG/ResourceAllocator/src/Allocateur.java
+++ ALG/ResourceAllocator/src/Allocateur.java
@@ -12,31 +12,31 @@
 
    Completer egalement les commentaires associes a chaque methode si
    necessaire.
-*/
+ */
 
 import java.util.*;
 
 public class Allocateur {
 
-    // attributs
+	// attributs
 
 	private Graphe _alloc;
-    private int max_proc; // nombre maximum de processus
-    private int nb_proc; // nombre effectif de processus
-    private int nb_ress;
+	private int max_proc; // nombre maximum de processus
+	private int nb_proc; // nombre effectif de processus
+	private int nb_ress;
 
-    // constructeurs
+	// constructeurs
 
-    /**
+	/**
        Creation d'un allocateur pouvant gerer jusqu'a 'nmaxProc' processus (numerotes de 0 a (nmaxProc-1))
        et exactement 'nRess' ressources (numerotees de 0 a (nRess-1).
-    */
-    public Allocateur(int nmaxProc, int nRess) {
-    	max_proc = nmaxProc;
-    	nb_proc = 0;
-    	nb_ress = nRess;
+	 */
+	public Allocateur(int nmaxProc, int nRess) {
+		max_proc = nmaxProc;
+		nb_proc = 0;
+		nb_ress = nRess;
 		_alloc = new GrapheDicoSucc(nmaxProc+nRess);
-		
+
 		//À la création du graphe, nous créons un sommet par ressource.
 		for(int i = 0; i < nRess; ++i)
 		{
@@ -46,89 +46,104 @@ public class Allocateur {
 				System.out.println("Erreur lors de la création de la ressource");
 			}
 		}
-    }
+	}
 
-    // methodes
+	// methodes
 
-    /**
+	/**
        Retourne vrai si et seulement si 'p' est un processus connu de l'allocateur
-     * @throws Exception 
-    */
-    public boolean validProc(int p) throws Exception {
-	return _alloc.validSom(p);
-    }
+	 * @throws Exception 
+	 */
+	public boolean validProc(int p) throws Exception {
+		return _alloc.validSom(p);
+	}
 
-    /**
+	/**
        Retourne vrai si et seulement si 'r' est une ressource connue de l'allocateur
-     * @throws Exception 
-    */
-    public boolean validRess(int r) throws Exception {
-	return _alloc.validSom(r);
-    }
+	 * @throws Exception 
+	 */
+	public boolean validRess(int r) throws Exception {
+		return _alloc.validSom(r);
+	}
 
-    /**
+	/**
        O1: Creation du nouveau processus 'p'.
-    */
-    public void creerProc(int p) {
-	// a completer
-	System.out.println("Creation du processus " + p);
-    }
+	 */
+	public void creerProc(int p) {
+		try {
+			if(validProc(nb_ress + p))
+			{
+				System.out.println("Le processus a déjà été créé");
+				return;
+			}
+		} catch (Exception e1) {
+			// TODO Bloc catch généré automatiquement
+			e1.printStackTrace();
+		}
+		try {
+			_alloc.ajoutSom(nb_ress + p);
+			nb_proc++;
+			System.out.println("Creation du processus " + p);
+		} catch (Exception e) {
+			System.out.println("Erreur lors de la création du processus");
+		}
+	}
 
-    /**
+	/**
        O2: Destruction du processus 'p'.
-    */
-    public void detruireProc(int p) {
-	// a completer
-	System.out.println("Destruction du processus " + p);
-    }
+	 */
+	public void detruireProc(int p) {
+		// a completer
+		System.out.println("Destruction du processus " + p);
+	}
 
-    /**
+	/**
        O3: Demande d'un ensemble de ressources 'lr' par le processus 'p'.
-    */
-    public void demanderRess(int p, Set lr) {
-	// a completer
-	System.out.println("Demande de " + lr.size() + " ressource(s) par le processus " + p);
-    }
+	 */
+	public void demanderRess(int p, Set lr) {
+		// a completer
+		System.out.println("Demande de " + lr.size() + " ressource(s) par le processus " + p);
+	}
 
-    /**
+	/**
        O4: Liberation d'une ressource 'r' par le processus 'p'.
-    */
-    public void libererRess(int p, int r) {
-	// a completer
-	System.out.println("Liberation de la ressource " + r + " par le processus " + p);
-    }
+	 */
+	public void libererRess(int p, int r) {
+		// a completer
+		System.out.println("Liberation de la ressource " + r + " par le processus " + p);
+	}
 
-    /**
+	/**
        O5: Affichage des files d'attentes des ressources.
-    */
-    public void afficherFiles() {
-	// a completer
-	System.out.println("Affichage des files d'attentes");
-    }
+	 */
+	public void afficherFiles() {
+		// a completer
+		System.out.println("Affichage des files d'attentes");
+	}
 
-    /**
+	/**
        O6: Affichage des processus actifs.
-    */
-    public void afficherActifs() {
-	// a completer
-	System.out.println("Affichage des processus actifs");
-    }
+	 */
+	public void afficherActifs() {
+		// a completer
+		System.out.println("Affichage des processus actifs");
+	}
 
-    /**
+	/**
        O7: Affichage des attentes entre processus.
-    */
-    public void afficherAttentes() {
-	// a completer
-	System.out.println("Affichage des attentes entre processus");
-    }
+	 */
+	public void afficherAttentes() {
+		// a completer
+		System.out.println("Affichage des attentes entre processus");
+	}
 
-    /**
+	/**
        O8: Affichage des interblocages.
-    */
-    public void afficherInterblocages() {
-	// a completer
-	System.out.println("Affichage des interblocages");
-    }
+	 */
+	public void afficherInterblocages() {
+		// a completer
+		System.out.println("Affichage des interblocages");
+	}
 
 } // class
 
