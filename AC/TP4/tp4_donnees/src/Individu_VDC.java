3c533dd2a7df4ddc94f85a1676f2b7833d5b00b2
AmarOk <blin.sebastien@enconn.fr>
2015-05-28 10:45:43 +0200

    begin TSP

--- AC/TP4/tp4_donnees/src/Individu_VDC.java
+++ AC/TP4/tp4_donnees/src/Individu_VDC.java
@@ -1,9 +1,37 @@
+import java.util.Random;
+
 public class Individu_VDC implements Individu {
+
+	private double[] _x;
+	private double[] _y;
+	private int[] _ind;
 
 	//Constructeur
 	public Individu_VDC(double[] coord_x, double[] coord_y) {
-		// TODO 
+		_x = coord_x;
+		_y = coord_y;
+		_ind = new int[_x.length];
+		for(int i = 0; i < _ind.length; ++i)
+		{
+			_ind[i] = i;
+		}
+		shuffleArray(_ind);
+		System.out.println(_ind);
 	}
+
+  // Implementing Fisher–Yates shuffle
+  static void shuffleArray(int[] ar)
+  {
+    Random rnd = new Random();
+    for (int i = ar.length - 1; i > 0; i--)
+    {
+      int index = rnd.nextInt(i + 1);
+      // Simple swap
+      int a = ar[index];
+      ar[index] = ar[i];
+      ar[i] = a;
+    }
+  }
 
 	/* Classes de l'interface Individu
 	 */
@@ -19,20 +47,26 @@ public class Individu_VDC implements Individu {
 	}
 	@Override
 	public void mutation(double prob) {
-		// TODO 
-		
+		for(int i = 0; i < _ind.length; ++i)
+			if(Math.random() < prob)
+			{
+				int r = Math.random()*_ind.length;
+				int temp = _ind[r];
+				_ind[r] = _ind[i];
+				_ind[i] = temp;
+			}
 	}
 	
 	/* Accesseurs (pour Display_VDC)
 	 */
 	public int[] get_parcours(){
-		return null;
+		return _ind;
 	}
 	public double[] get_coord_x(){
-		return null;
+		return _x;
 	}
 	
 	public double[] get_coord_y(){
-		return null;
+		return _y;
 	}	
 }
